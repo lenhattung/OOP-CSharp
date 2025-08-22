f66c8699155d91b030b1508f75c2558290dd2b97
lenhattung <lenhattung@gmail.com>
2025-08-22 20:21:36 +0700

    u
    u

---
+++ Bai 01d/Program.cs
@@ -0,0 +1,32 @@
+﻿// Xac dinh chi so BMI cua mot nguoi
+// Can nang va chieu cao (nhap tu ban phim)
+// BMI = Cân nặng/Chiều cao^2 , kg va m
+
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+
+        double canNang = 0;
+        double chieuCao = 0;
+
+        Console.Write("Nhập vào cân nặng (kg): ");
+        canNang = double.Parse(Console.ReadLine());
+
+        Console.Write("Nhập vào chiều cao (m): ");
+        chieuCao = double.Parse(Console.ReadLine());
+
+        double bmi = canNang/(chieuCao*chieuCao);
+
+        Console.WriteLine("BMI của bạn là : {0}", bmi);
+
+        Console.WriteLine("Nhập phím bất kỳ để thoát.");
+        Console.ReadKey();
+    }
+}
+
+
---
+++ Bai 02/Program2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    enum NhietDoNuoc: int
+    {
+        DoDong = 0,
+        DoNguoi = 20,
+        DoAm = 40,
+        DoNong = 60,
+        DoSoi = 100
+    }
+
+    public static void Main1()
+    {
+        NhietDoNuoc nhietDo = NhietDoNuoc.DoNguoi;
+        Console.WriteLine($"Tên: {nhietDo}");
+        Console.WriteLine($"Giá trị: {(int)nhietDo}");
+    }
+
+}
