82dfb1829edf8e8c0f06eb4705dc3658bd83fcd7
lenhattung <lenhattung@gmail.com>
2025-09-09 19:43:33 +0700

    u

--- Bai 03 - OOP/obj/Debug/net8.0/Bai 03 - OOP.AssemblyInfo.cs
+++ Bai 03 - OOP/obj/Debug/net8.0/Bai 03 - OOP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai 03 - OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a82f21bfe83d4b73ffc71ff1c4a43250409af1af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c00f9f351e7151f5f6a6772b132c6ab381ae28bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai 03 - OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai 03 - OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
