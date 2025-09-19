5e552a0c76ca6e2038670437b1387b0c9e937d59
lenhattung <lenhattung@gmail.com>
2025-09-19 14:31:10 +0700

    u
    u

--- Bai 04 - OOP - Ke thua (Inheritance)/Program.cs
+++ Bai 04 - OOP - Ke thua (Inheritance)/Program.cs
@@ -8,7 +8,7 @@ class Program
         Tester t1 = new Tester("Tung", 1, 5000000, "Dev", "Selenium", 100);
         Manager m1 = new Manager("Tung", 1, 5000000, "MN", 5000000, 100);
 
-        // Tính đa hình 
+        // Tính đa hình  - Polymorphism 
         Employee d2 = new Developer("Tung", 1, 5000000, "Dev", "C#", 10);
         Employee t2 = new Tester("Tung", 1, 5000000, "Dev", "Selenium", 100);
         Employee m2 = new Manager("Tung", 1, 5000000, "MN", 5000000, 100);
