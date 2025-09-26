00436909076f80226dc561a7358f911bab9c53f6
lenhattung <lenhattung@gmail.com>
2025-09-26 16:08:44 +0700

    u
    u

---
+++ Bai 07 - Operator Overloading/Program.cs
@@ -0,0 +1,36 @@
+﻿Vector2D v1 = new Vector2D(3, 4);
+Vector2D v2 = new Vector2D(1, 2);
+
+Vector2D sum = v1 + v2;        // Kết quả: (4, 6)
+Vector2D diff = v1 - v2;       // Kết quả: (2, 2)
+Vector2D scaled = v1 * 2;      // Kết quả: (6, 8)
+Vector2D scaled2 = 3 * v1;     // Kết quả: (9, 12)
+
+Console.WriteLine($"v1 + v2 = {sum}");
+Console.WriteLine($"v1 - v2 = {diff}");
+Console.WriteLine($"v1 * 2 = {scaled}");
+
+Console.WriteLine(v1 == v2);
+
+
+Vector2D v3 = new Vector2D(3, 4);
+Vector2D v4 = new Vector2D(3, 4);
+Vector2D v5 = new Vector2D(1, 2);
+
+Console.WriteLine(v3 == v4); // True
+Console.WriteLine(v4 != v5); // True
+
+/*
++ Toán tử BẮT BUỘC cặp đôi:
+    == và !=
+    > và <
+    >= và <=
+    true và false
+
++ Toán tử KHÔNG thể nạp chồng:
+    = (gán)
+    &&, || (logic)
+    [] (indexer - có cách khác)
+    () (cast - có cách khác)
+    new, is, as, typeof
+*/
---
+++ Bai 07 - Operator Overloading/Vector2D.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Vector2D
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public Vector2D(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    // Overloading +
+    public static Vector2D operator +(Vector2D v1, Vector2D v2)
+    {
+        return new Vector2D(v1.X+v2.X, v1.Y+v2.Y);
+    }
+
+    // Overloading -
+    public static Vector2D operator -(Vector2D v1, Vector2D v2)
+    {
+        return new Vector2D(v1.X - v2.X, v1.Y - v2.Y);
+    }
+
+    // Nạp chồng toán tử nhân với số (*) 
+    public static Vector2D operator *(Vector2D v, double scalar)
+    {
+        return new Vector2D(v.X * scalar, v.Y * scalar);
+    }
+
+    // Nạp chồng toán tử nhân với số (thứ tự ngược lại)
+    public static Vector2D operator *(double scalar, Vector2D v)
+    {
+        return new Vector2D(v.X * scalar, v.Y * scalar);
+    }
+    // Override ToString để hiển thị đẹp
+    public override string ToString()
+    {
+        return $"({X}, {Y})";
+    }
+
+    // Nạp chồng toán tử bằng (==)
+    public static bool operator ==(Vector2D v1, Vector2D v2)
+    {
+        if (ReferenceEquals(v1, v2)) return true;
+        if (v1 is null || v2 is null) return false;
+        return v1.X == v2.X && v1.Y == v2.Y;
+    }
+
+
+    // Nạp chồng toán tử khác (!=) - BẮT BUỘC khi có ==
+    public static bool operator !=(Vector2D v1, Vector2D v2)
+    {
+        return !(v1 == v2);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is Vector2D other)
+            return this == other;
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return X.GetHashCode() ^ Y.GetHashCode();
+    }
+}
