e29c04c23a34fd32c574fc81567e2da3d3f72dd4
lenhattung <lenhattung@gmail.com>
2025-10-14 18:54:53 +0700

    u
    u

---
+++ Bai 12 - Collections - List/ListExample.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+public class ListExample
+{
+
+    public static void Main()
+    {
+        // Sử dụng mảng
+        Student[] danhSachSV = new Student[35];
+        danhSachSV[0] = new Student(01, "NVA", 9.0);
+
+
+        // Tạo và sử dụng danh sách
+        ArrayList students = new ArrayList();
+        students.Add(new Student(01, "NVA", 9.0));
+        for(int i = 10; i < 100; i++)
+        {
+            students.Add(new Student(i, "Nguyen "+i, i));
+        }
+
+
+        students.RemoveAt(0);
+
+
+        foreach (var item in students)
+        {
+            Console.WriteLine(item.ToString());
+        }
+
+
+        Console.ReadKey();
+
+    }
+}
---
+++ Bai 12 - Collections - List/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public double GPA { get; set; }
+
+    public Student(int id, string Name, double GPA)
+    {
+        this.Id = id;
+        this.Name = Name;
+        this. GPA = GPA;
+    }
+
+    public override string ToString()
+    {
+        return $"ID: {Id}, Name: {Name}, GPA: {GPA}";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return base.Equals(obj);
+    }
+}
+
