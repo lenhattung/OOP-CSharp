fa1644606e4a7b7534d748791607e046a0f661f7
lenhattung <lenhattung@gmail.com>
2025-10-17 19:24:20 +0700

    u
    u

--- Bai 12 - Collections - List/obj/Debug/net8.0/Bai 12 - Collections - List.AssemblyInfo.cs
+++ Bai 12 - Collections - List/obj/Debug/net8.0/Bai 12 - Collections - List.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bai 12 - Collections - List")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96b768f62a18576058d36ac8b0883cc01151d142")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e377ca0a4db634676bd8c57e092013935abed52b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bai 12 - Collections - List")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bai 12 - Collections - List")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
