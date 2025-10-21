303a4a52e8da49498dd8481913427f199d7848cb
lenhattung <lenhattung@gmail.com>
2025-10-21 19:55:35 +0700

    u
    u

---
+++ Bai 14 - Collection - Dictionary/Form1.cs
@@ -0,0 +1,10 @@
+namespace Bai_14___Collection___Dictionary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
